45e3c76a5d663be950b3eea7ef3698bf41fc668d
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2025-10-27 23:33:47 -0700

    Add calib status (4.3.54), power mode (3.5), etc

--- Drivers/BNO055/BNO055.cc
+++ Drivers/BNO055/BNO055.cc
@@ -1,18 +1,51 @@
 
 #include "BNO055.h"
 
+bno055::bno055()
+{
+    /* TODO: do whatever needed for the i2c hal wrapper */
+}
+
 bno055::result bno055::connect(int sda, int sdl, char addr)
 {
     /* TODO: initialize i2c hal wrapper */
-    if (read_byte(bno055::CHIP_ID_ADDR) != bno055::CHIP_ID) {
-        return bno055::result::UNCONNECTED;
+    if (read_byte(CHIP_ID_ADDR) != CHIP_ID)
+        return result::UNCONNECTED;
+    set_page(0);
+
+    return result::OK;
+}
+
+void bno055::set_page(int page)
+{
+    write_byte(PAGE_ID_ADDR, (uint8_t) page);
+}
+
+bool bno055::is_calibrated(sensor sensor)
+{
+    uint8_t calib_stat = read_byte(CALIB_STAT_ADDR);
+
+    // see 4.3.54 
+    switch (sensor) {
+    case sensor::SYSTEM: // bits <7:6>
+        return ((calib_stat >> 6) & 0x03) != 3;
+    case sensor::GYROSCOPE: // bits <5:4>
+        return ((calib_stat >> 4) & 0x03) != 3;
+    case sensor::ACCELEROMETER: // bits <3:2>
+        return ((calib_stat >> 2) & 0x03) != 3;
+    case sensor::MAGNETOMETER: // bits <1:0>
+        return (calib_stat & 0x03) != 3;
     }
-    return bno055::result::OK;
+}
+
+void bno055::set_power_mode(pwr_mode mode)
+{
+    write_byte(PWR_MODE_ADDR, (uint8_t) mode);
 }
 
 uint8_t bno055::read_byte(uint8_t address)
 {
-    /* TODO: read i2c using hal wrapper */
+    // TODO: read i2c using hal wrapper
     return 0;
 }
 
@@ -22,4 +55,9 @@ uint16_t bno055::read_short(uint8_t lsb, uint8_t msb)
     out |= read_byte(lsb);
     out |= ((uint16_t) read_byte(msb)) << 8;
     return out;
+}
+
+void bno055::write_byte(uint8_t address, uint8_t value)
+{
+    // TODO: write i2c using hal wraper
 }
--- Drivers/BNO055/BNO055.h
+++ Drivers/BNO055/BNO055.h
@@ -4,41 +4,128 @@
 
 #include <stdint.h>
 
+/**
+ * A class representing the driver for the BNO055 absolute orientation sensor.
+ *
+ * To use the driver, use the #connect method to initialize the connection with
+ * the sensor.
+ */
 class bno055 {
-public:
-    static constexpr uint8_t CHIP_ID_ADDR = 0x00;
+public: // constants
+    static constexpr uint8_t CHIP_ID_ADDR = 0x00; /* Chip identification code */
+    static constexpr uint8_t PAGE_ID_ADDR = 0x07; /* Page Id */
+    static constexpr uint8_t CALIB_STAT_ADDR = 0x35; /* Calibration status */
+    static constexpr uint8_t SYS_STATUS_ADDR = 0x39; /* System status code */
+    static constexpr uint8_t PWR_MODE_ADDR = 0x3E; /* Power mode */
 
-    /** expected fixed-value of CHIP_ID (0x00) register */
+    /** expected fixed-value of CHIP_ID (0x00) register (see 4.3.1). */
     static constexpr uint8_t CHIP_ID = 0xA0;
 
-public:
+public: // types
+    /**
+     * Enum representing different error conditions with the driver.
+     */
     enum class result {
         OK,
         UNCONNECTED,
     };
 
+    /**
+     * Enum representing the different sensors within the device.
+     */
+    enum class sensor {
+        SYSTEM,
+        GYROSCOPE,
+        ACCELEROMETER,
+        MAGNETOMETER,
+    };
+
+    /** Vector output type */
     struct vec4 {
         double x, y, z, w;
     };
 
-public:
+    /**
+     * Enum representing the different system status code values (see 4.3.58).
+     */
+    enum class sys_status_result { 
+        SYSTEM_IDLE = 0, /* System Idle */
+        SYSTEM_ERROR = 1, /* System Error */
+    };
+
+    /**
+     * Enum representing the different operation modes (see 3.5).
+     */
+    enum class opr_mode {
+
+    };
+
+    /**
+     * Enum representing the different power modes (see 3.2).
+     */
+    enum class pwr_mode {
+        NORMAL = 0x00,
+        LOW_POWER = 0x01,
+        SUSPEND = 0x02,
+        INVALID = 0x03,
+    };
+
+public: // methods
 
     bno055();
+
+    /**
+     * Initializes a connection to the BNO055 using the given I2C pins and slave
+     * address. For reference, the BNO055 uses a slave address of 0x29 by
+     * default.
+     *
+     * Returns result::OK if the driver connected successfully. Otherwise,
+     * returns result::UNCONNECTED.
+     */
     result connect(int sda, int sdl, char addr);
 
-    
+    /**
+     * Sets the current register map page of the device (see 4.3.8).
+     */
+    void set_page(int page);
+
+    /**
+     * Queries and checks the calibration status of one of the sensors of the
+     * device (see 3.11.1 and 4.3.54).
+     *
+     * Returns true if the sensor is fully calibrated. Otherwise, returns false.
+     */
+    bool is_calibrated(sensor sensor);
+
+    /**
+     * Sets the power mode of the chip to the given power mode (see 3.2 and
+     * 4.3.62).
+     */
+    void set_power_mode(pwr_mode mode);
+
+    /**
+     * Sets the power mode to suspend (see 3.2.3). All sensors are put into
+     * sleep mode.
+     */
+    void suspend();
 
 private:
 
     /**
-     * read an unsigned byte (8-bits) using the register address
+     * Read an unsigned byte (8-bits) using the register address.
      */
     uint8_t read_byte(uint8_t address);
+
     /**
-     * read an unsigned short (16-bits) using the register addresses of the
-     * least significant bits and the most significant bits
+     * Read an unsigned short (16-bits) using the register addresses of the
+     * least significant bits and the most significant bits.
      */
     uint16_t read_short(uint8_t lsb, uint8_t msb);
+
+    /**
+     * Write an unsigned byte (8-bits) of given value to given register address.
+     */
+    void write_byte(uint8_t address, uint8_t value);
 
 };
 
