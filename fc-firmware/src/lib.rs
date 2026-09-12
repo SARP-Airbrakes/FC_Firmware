5ce73fa82609c7790bdff57285f4b7d939d64d18
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2026-09-11 21:46:52 -0700

    docs: FC firmware crate-level documentation

--- fc-firmware/src/lib.rs
+++ fc-firmware/src/lib.rs
@@ -1,14 +1,33 @@
+//! This library comprises the initialization of the Airbrakes flight computer
+//! hardware.
+//!
+//! This includes the clock configuration and each of the peripherals including
+//! the [`bmp390`], [`bmi088`], and [`w25qxxxjv`] (in the form of a W25Q128JV on
+//! board). The intention with this library is to provide a way for both the
+//! firmware (`main.rs`) and the integration tests (`tests/`) to initialize the
+//! hardware.
+//!
+//! For information regarding building and running the firmware, please see the
+//! folder-level documentation (`fc-firmware/README.md`).
+
 #![no_std]
 #![no_main]
-
-//! This library comprises all setup boilerplate for the airbrakes flight
-//! computer hardware.
 
 pub mod log;
 
 use defmt::unwrap;
 use embassy_stm32::{
-    Config, Peri, Peripherals, dma, gpio, i2c::{self, I2c}, interrupt::typelevel, mode, peripherals::*, spi::{self, Spi}, time::{khz, mhz}
+    Config, 
+    Peri, 
+    Peripherals, 
+    dma, 
+    gpio, 
+    i2c::{self, I2c}, 
+    interrupt::typelevel, 
+    mode, 
+    peripherals::*, 
+    spi::{self, Spi}, 
+    time::{khz, mhz}
 };
 use embassy_time::Timer;
 use embassy_sync::mutex::Mutex;
@@ -18,23 +37,31 @@ use static_cell::StaticCell;
 
 use defmt_rtt as _;
 
+/// An I2C device that is synchronized between several Embassy tasks.
 pub type I2cBus = Mutex<CriticalSectionRawMutex, I2c<'static, mode::Async, i2c::mode::Master>>;
 
 /// Initializes the [`embassy_stm32`] HAL with the flight computer clock
 /// configuration.
+///
+/// This specifically initializes the HSE to be a 16MHz oscillator (as the
+/// latest revision of the PCB is setup) and for no LSE to be present. It also
+/// ensures that all of the peripheral clocks are at a stable 16MHz sourced 
+/// from the HSE.
 pub fn setup_stm32() -> Peripherals {
     let mut cfg = Config::default();
 
     // Configure clocks
+    // This is closely matched with the clock configuration from the original
+    // C++ firmware, as solved by the STM32CubeMX configuration.
     {
         use embassy_stm32::rcc::*;
 
-        // Closely matched with the solved configuration from CubeMX
         cfg.rcc.hse = Some(Hse {
             freq: mhz(16),
             mode: HseMode::Oscillator,
         });
 
+        // Source the peripheral clocks from the HSE
         cfg.rcc.pll_src = PllSource::HSE;
         cfg.rcc.pll = Some(Pll {
             prediv: PllPreDiv::DIV8,
@@ -55,6 +82,15 @@ pub fn setup_stm32() -> Peripherals {
 }
 
 /// Sets up the W25Q128JV connected to the board.
+/// 
+/// Configures the given SPI bus and the DMA (specifically DMA2 on channels 2
+/// and 3) to be used solely for interfacing with the W25Q128JV. The parameters
+/// are specific to the latest revision of the PCB.
+///
+/// # Panics
+/// This function will panic if the W25Q128JV is not correctly connected, and
+/// the device id could not be read or is incorrect (different model). This
+/// function will also panic if called more than one time.
 pub async fn initialize_w25q128jv<D>(
     spi: Peri<'static, SPI1>,
     sck: Peri<'static, PA5>,
@@ -72,6 +108,7 @@ where
 {
     static DELAY_CELL: StaticCell<embassy_time::Delay> = StaticCell::new();
 
+    // Configure with a 1MHz frequency
     let config = {
         let mut config = spi::Config::default();
         config.frequency = mhz(1);
@@ -103,6 +140,16 @@ where
     w25q128jv
 }
 
+/// Initializes the I2C bus for usage with the two peripheral driver
+/// implementations.
+///
+/// Creates a synchronization primitive ([`embassy_sync::mutex::Mutex`], see
+/// [`I2cBus`]) to share the I2C device between Embassy tasks safely. See also
+/// [`embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice`] for an
+/// implementation of [`embedded_hal_async::i2c::I2c`] to use the [`I2cBus`].
+///
+/// # Panics
+/// This function will panic if called more than once.
 pub async fn initialize_i2c_bus<D>(
     i2c: Peri<'static, I2C1>,
     mut scl: Peri<'static, PB8>,
@@ -140,6 +187,7 @@ where
 }
 
 #[cfg(test)]
+#[doc(hidden)]
 #[embedded_test::tests]
 mod tests {
     // This has to be here to avoid linker issues.
--- fc-firmware/src/main.rs
+++ fc-firmware/src/main.rs
@@ -1,3 +1,14 @@
+//! This is firmware for the Airbrakes flight computer.
+//!
+//! There are, in total, three Embassy executors: a thread executor (see
+//! [`EXECUTOR_CLI`]), and two interrupt executors ([`EXECUTOR_LOW`] at priority
+//! 6, and [`EXECUTOR_HIGH`] at priority 7). The thread executor has the sole
+//! purpose of for processing CLI and processing commands. The low priority
+//! executor is for the flight log, and the high priority executor is for the
+//! USB, sensors, and controller.
+//!
+//! For information regarding running and testing the firmware, please see the
+//! folder-level `README.md`.
 
 #![no_std]
 #![no_main]
@@ -23,10 +34,14 @@ use crate::cli::process_cli;
 use crate::memory::initialize_memory;
 use crate::sensor::initialize_i2c;
 
+/// The high-priority executor, for the USB, sensors and controller.
 static EXECUTOR_HIGH: InterruptExecutor = InterruptExecutor::new();
+/// The low-priority executor for the flight log.
 static EXECUTOR_LOW: InterruptExecutor = InterruptExecutor::new();
+/// The thread-mode executor for the CLI.
 static EXECUTOR_CLI: StaticCell<Executor> = StaticCell::new();
-
+ 
+/// Spare interrupt used for the low priority interrupt executor.
 #[interrupt]
 fn SPI2() {
     unsafe {
@@ -34,6 +49,7 @@ fn SPI2() {
     }
 }
 
+/// Spare interrupt used for the high priority interrupt executor.
 #[interrupt]
 fn SPI3() {
     unsafe {
@@ -41,6 +57,7 @@ fn SPI3() {
     }
 }
 
+/// Task to setup and process the USB CDC ACM device.
 #[embassy_executor::task]
 async fn process_usb(
     usb: Peri<'static, USB_OTG_FS>,
@@ -50,12 +67,15 @@ async fn process_usb(
     usb::setup_usb(usb, dp, dm).await;
 }
 
+/// Entry-point of the flight computer firmware.
 #[entry]
 fn main() -> ! {
+    // Initialize peripherals.
     let p = fc_firmware::setup_stm32();
 
     debug!("Starting flight firmware.");
 
+    // Initialize the low-priority executor.
     interrupt::SPI2.set_priority(interrupt::Priority::P6);
     let low_spawner = EXECUTOR_LOW.start(interrupt::SPI2);
     low_spawner.spawn(unwrap!(initialize_memory(
@@ -68,11 +88,13 @@ fn main() -> ! {
         p.PA9
     )));
 
+    // Initialize the high-priority executor.
     interrupt::SPI3.set_priority(interrupt::Priority::P7);
     let high_spawner = EXECUTOR_HIGH.start(interrupt::SPI3);
     high_spawner.spawn(unwrap!(process_usb(p.USB_OTG_FS, p.PA12, p.PA11)));
     high_spawner.spawn(unwrap!(initialize_i2c(high_spawner, p.I2C1, p.PB8, p.PB9, p.DMA1_CH6, p.DMA1_CH0)));
-
+ 
+    // Initialize and run the thread-mode executor.
     EXECUTOR_CLI
         .init_with(Executor::new)
         .run(|spawner| {
